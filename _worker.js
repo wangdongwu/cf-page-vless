82f918c8d756ba183fa14c1d4a5ad05b0a20b2ce
王东武 <496414500@qq.com>
2024-11-12 09:26:48 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@
 import { connect } from 'cloudflare:sockets';
 
 // 建议修改为自己的 UUID
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '853d5c11-168f-4dad-8e5e-a6784cc13861';
 
 // 生成配置文件的 CF 优选 IP
 const bestCFIP = "www.gov.se"
